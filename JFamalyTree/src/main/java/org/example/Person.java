c70b01c8342f9b6a33399d88a6de2e012c2884f2
Alexandr <jwchaganav@gmail.com>
2023-03-03 15:55:23 +0400

    Person

---
+++ JFamalyTree/src/main/java/org/example/Person.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class Person {
+    private String name;
+    private int age;
+    private String gender;
+    public Person(){
+        name = "Неизвестный";
+        age = 0;
+        gender = "М";
+    }
+    public Person(String name){
+        this();
+        this.name = name;
+    }
+    public Person(String name, int age){
+        this();
+        this.name = name;
+        this.age = age;
+    }
+    public Person(String name, int age, String gender){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
