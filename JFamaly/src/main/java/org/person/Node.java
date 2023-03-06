6606a91c47d493fcc036f0d336dc54be45d1fac0
Alexandr <jwchaganav@gmail.com>
2023-03-06 11:06:50 +0400

    Node

--- JFamaly/src/main/java/org/person/Node.java
+++ JFamaly/src/main/java/org/person/Node.java
@@ -1,7 +1,22 @@
 package org.person;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Node {
     Person p1;
     Relation relation;
     Person p2;
+    List<Node> nodes = new ArrayList<>();
+    public Node(Person p1, Relation rel, Person p2){
+        this.p1 = p1;
+        this.relation = rel;
+        this.p2 = p2;
+    }
+    public void addNode(Node node){
+        nodes.add(node);
+    }
+    public List<Node> getNodes(Relation rel){
+        return nodes;
+    }
 }
