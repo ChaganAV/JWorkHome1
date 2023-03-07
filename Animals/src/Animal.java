0e6bbb4f0af957237773c807dea0125b059e1703
Alexandr <jwchaganav@gmail.com>
2023-03-07 14:22:45 +0400

    animal

---
+++ Animals/src/Animal.java
@@ -0,0 +1,11 @@
+public abstract class Animal {
+    private String genus;
+    public Animal(String genus){
+        this.genus = genus;
+    }
+    public Animal(){
+    }
+    public void say(){
+        System.out.println("ku");
+    }
+}
